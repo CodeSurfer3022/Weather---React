5c2707af3623ddf678c5789adb68a3d751e78e54
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-10 22:25:30 +0530

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component{
     }
     render() {
         return (
-            <div>
+            <div id="weather-card">
                 {this.getUrl('london')}
             </div>
         )
