e67ada41f432f510100e8aefa81f85338ccea18c
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-10 21:22:34 +0530

    Add functions to get weather data from openWeather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class App extends React.Component{
+    async getWeatherData(url) {
+        try {
+            const response = await fetch(url, {mode: 'cors'});
+            const data = await response.json();
+            console.log(data);
+        } catch(error) {
+
+        }
+    }
+    getUrl(location) {
+        const city = location;
+        const api_key = "fb94cdfd6fbdf9e769da8febcdcf4fba";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${api_key}`;
+        this.getWeatherData(url);
+    }
     render() {
         return (
             <div>
+                {this.getUrl('london')}
             </div>
         )
     }
