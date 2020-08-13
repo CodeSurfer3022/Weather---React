bdf25857eacfb32508943ce2f36b17baf6fae7c3
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-08-13 16:54:26 +0530

    get info object from data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Info from './Info';
 
+function getDegreesinCandF(kelvin) {
+        return {
+            centigrade: Math.round(kelvin - 273.15),
+            fahrenheit: Math.round((kelvin - 273.15) * 9 / 5 + 32)
+        }
+}
+
 class App extends React.Component{
     getExtraInfo(data) {
+        console.log(data);
         return data;
     }
     getInfo(data) {
-        return data;
+        let temp = data.main.temp;
+        let feelsLike = data.main.feels_like;
+        return {
+            cityName: data.name,
+            clouds: data.weather[0].description,
+            temperature: getDegreesinCandF(temp),
+            feelsLike: getDegreesinCandF(feelsLike),
+        };
     }
     async getWeatherData(city) {
         try {
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${process.env.REACT_APP_API_KEY}`;
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.REACT_APP_API_KEY}`;
             const response = await fetch(url, {mode: 'cors'});
             const data =  await response.json();
-            this.getInfo(data);
+            const info = this.getInfo(data);
+            console.log(info);
             this.getExtraInfo(data);
         } catch(error) {
 
@@ -21,7 +37,7 @@ class App extends React.Component{
     }
 
     render() {
-        this.getWeatherData('london');
+        this.getWeatherData('seattle');
         return (
             <div id="weather-card">
                 <div>
