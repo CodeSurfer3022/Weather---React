5e11c4d33a674e8c0bb9a541de7ab19de2203db0
Chandra Prakash <pr.chandraprakash@gmail.com>
2021-03-09 18:05:10 +0530

    Move extra functions outside and remove sky component

--- src/App.js
+++ src/App.js
@@ -9,41 +9,43 @@ function getDegreesinCandF(kelvin) {
   }
 }
 
+function getExtraInfo(data) {
+  return {
+    sunrise: data.sys.sunrise,
+    sunset: data.sys.sunset,
+    humidity: data.main.humidity,
+    pressure: data.main.pressure,
+    visibility: data.visibility,
+    wind: data.wind,
+    min_temp: data.main.temp_min,
+    max_temp: data.main.temp_max
+  }
+}
+
+function getInfo(data) {
+  let temp = data.main.temp;
+  let feelsLike = data.main.feels_like;
+  return {
+    cityName: data.name,
+    country: data.sys.country,
+    clouds: data.weather[0].description,
+    cloudsImage: data.weather[0].icon,
+    temperature: getDegreesinCandF(temp),
+    feelsLike: getDegreesinCandF(feelsLike),
+  };
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.getWeatherData = this.getWeatherData.bind(this);
+    this.searchWeatherData = this.searchWeatherData.bind(this);
   }
 
   componentDidMount() {
     this.getWeatherData('Bengaluru').then(data => {
       this.setState(data);
     });
-  }
-
-  getExtraInfo(data) {
-    return {
-      sunrise: data.sys.sunrise,
-      sunset: data.sys.sunset,
-      humidity: data.main.humidity,
-      pressure: data.main.pressure,
-      visibility: data.visibility,
-      wind: data.wind,
-      min_temp: data.main.temp_min,
-      max_temp: data.main.temp_max
-    }
-  }
-
-  getInfo(data) {
-    let temp = data.main.temp;
-    let feelsLike = data.main.feels_like;
-    return {
-      cityName: data.name,
-      country: data.sys.country,
-      clouds: data.weather[0].description,
-      cloudsImage: data.weather[0].icon,
-      temperature: getDegreesinCandF(temp),
-      feelsLike: getDegreesinCandF(feelsLike),
-    };
   }
 
   async getWeatherData(city) {
@@ -53,8 +55,8 @@ class App extends Component {
       const data = await response.json();
       console.log(data);
       return {
-        info: this.getInfo(data),
-        extraInfo: this.getExtraInfo(data)
+        info: getInfo(data),
+        extraInfo: getExtraInfo(data)
       }
     } catch (error) {
       this.setState({});
@@ -69,18 +71,18 @@ class App extends Component {
   }
 
   render() {
-    console.log(this.state)
     if (!this.state) {
       return <div>Loading</div>
     }
+
     if (Object.keys(this.state).length === 0 && this.state.constructor === Object) {
       return <div>couldn't find the city</div>
     }
-    const info = this.state.info;
+
     return (
       <div id="screen">
-        <Header />
-        <WeatherCard />
+        <Header searchWeatherData={this.searchWeatherData}/>
+        <WeatherCard info={this.state.info}/>
       </div>
     );
   }
